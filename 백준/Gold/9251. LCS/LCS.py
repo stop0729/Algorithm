753cb27eb2f09fc82039cc7c4342e657246224c9
stop0729 <78136790+stop0729@users.noreply.github.com>
2025-03-09 01:18:44 +0900

    [Gold V] Title: LCS, Time: 112 ms, Memory: 118548 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,13 @@
+a = input()
+b = input()
+
+lcs = [[0] * (len(b) +1) for _ in range(len(a) + 1)]
+
+for i in range(1, len(a)+1):
+    for j in range(1, len(b)+1):
+        if a[i-1] == b[j-1]:
+            lcs[i][j] = lcs[i-1][j-1] + 1
+        else:
+            lcs[i][j] = max(lcs[i][j-1], lcs[i-1][j])
+
+print(lcs[len(a)][len(b)])
