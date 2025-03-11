5e85329614c524ad7acc9f851e3ef01929aa6fd8
stop0729 <78136790+stop0729@users.noreply.github.com>
2025-03-11 14:29:22 +0900

    [Gold IV] Title: 트리의 지름, Time: 308 ms, Memory: 39908 KB -BaekjoonHub

--- 백준/Gold/1967. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -1,5 +1,6 @@
 import sys
 import heapq
+sys.setrecursionlimit(10**6)
 
 inf = 1e9
 n = int(input())
@@ -11,28 +12,45 @@ for i in range(n-1):
     graph[b].append((a, c))
 
 
-def dijkstra(start):
-    q = []
-    distance = [inf] * (n+1)
-    distance[start] = 0
-    heapq.heappush(q, (0, start))
+# def dijkstra(start):
+#     q = []
+#     distance = [inf] * (n+1)
+#     distance[start] = 0
+#     heapq.heappush(q, (0, start))
     
-    while q:
-        dist, now = heapq.heappop(q)
-        if distance[now] < dist:
-            continue
-        for i in graph[now]:
-            cost = dist + i[1]
-            if distance[i[0]] > cost:
-                distance[i[0]] = cost
-                heapq.heappush(q, (cost, i[0]))
+#     while q:
+#         dist, now = heapq.heappop(q)
+#         if distance[now] < dist:
+#             continue
+#         for i in graph[now]:
+#             cost = dist + i[1]
+#             if distance[i[0]] > cost:
+#                 distance[i[0]] = cost
+#                 heapq.heappush(q, (cost, i[0]))
 
-    return distance[1:]
+#     return distance[1:]
 
-result = 0
-distance = dijkstra(1)
+# result = 0
+# distance = dijkstra(1)
+# temp = max(distance)
+# temp = distance.index(temp)
+# result = max(dijkstra(temp+1))
+
+distance = [-1] * (n+1)
+
+def dfs(node):
+    for i in graph[node]:
+        new_node, dist = i
+        if distance[new_node] == -1:
+            distance[new_node] = distance[node] + dist
+            dfs(new_node)
+
+distance[1] = 0
+dfs(1)
 temp = max(distance)
-temp = distance.index(temp)
-result = max(dijkstra(temp+1))
+idx = distance.index(temp)
 
-print(result)
+distance = [-1] * (n+1)
+distance[idx] = 0
+dfs(idx)
+print(max(distance))
