e599f45293f4ce8a45996e84924b0348d1b1f95f
stop0729 <78136790+stop0729@users.noreply.github.com>
2025-03-20 16:56:37 +0900

    [level 3] Title: 네트워크, Time: 1.19 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -0,0 +1,25 @@
+def dfs(n, start, data, visited):
+    for i in data[start]:
+        if visited[i] == False:
+            visited[i] = True
+            dfs(n, i, data, visited)
+    return visited
+    
+def solution(n, computers):
+    data = [[] for i in range(n+1)]
+    
+    for i in range(n):
+        for j in range(n):
+            if i != j and computers[i][j] == 1:
+                data[i+1].append(j+1)
+ 
+                
+    visited = [False] * (n+1)
+    answer = 0
+    
+    for start in range(1, n+1):
+        if visited[start] == False:
+            answer += 1
+            visited = dfs(n, start, data, visited)
+            
+    return answer
