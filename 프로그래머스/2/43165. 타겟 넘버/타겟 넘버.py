da3a33553bf64af09ab8c168e467769d709a99ff
stop0729 <78136790+stop0729@users.noreply.github.com>
2025-08-23 23:58:31 +0900

    [level 2] Title: 타겟 넘버, Time: 341.66 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,17 +1,18 @@
-from itertools import combinations
+answer = 0 
+
+def dfs(cnt, target, result, numbers):
+    global answer
+    if cnt == len(numbers):
+        if result == target:
+            answer += 1
+        return
+
+    dfs(cnt+1, target, result + numbers[cnt], numbers)
+    dfs(cnt+1, target, result - numbers[cnt], numbers)
+    
 
 def solution(numbers, target):
-    temp = [i for i in range(len(numbers))]
-    total = sum(numbers)
-    answers = 0
-    
-    for i in range(1, len(numbers)+1):
-        result = list(combinations(temp, i))
-        for j in result:
-            temp2 = 0
-            for k in j:
-                temp2 += numbers[k]
-            if total - temp2*2 == target:
-                answers += 1
 
-    return answers
+    dfs(0, target, 0, numbers)
+    
+    return answer
